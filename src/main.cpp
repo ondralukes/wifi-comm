383945d66b2741d775decf2e3e2e4e0efae8147b
Ondřej Lukeš <mail@ondralukes.cz>
2021-03-18 19:04:04 +0100

    Pin remapping

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
 #include "DeviceManager.h"
 #include "WiFiManager.h"
 #include "MessageBuilder.h"
@@ -10,31 +9,13 @@
 Display lcd;
 DeviceManager deviceManager(lcd);
 
-const uint8_t rowPins [4] = {10, D9, D3, D5};
-const uint8_t colsPins [4] = {D4, D6, D7, D8};
+const uint8_t rowPins [4] = {D4, D9, D3, D5};
+const uint8_t colsPins [4] = {D0, D6, D7, D8};
 Keyboard keyboard(rowPins, 4, colsPins, 4);
 void setup() {
     // Convert RX to GPIO
     pinMode(D9, FUNCTION_3);
     WiFi.mode(WIFI_OFF);
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    lcd.WriteRolling("test");
-}
-
-
-void status_led(){
-    if(WiFiManager::Status() == Station){
-        digitalWrite(LED_BUILTIN, LOW);
-        return;
-    }
-    static unsigned long prevBlink = 0;
-    static bool state = false;
-    if(millis() - prevBlink > 250){
-        state = !state;
-        prevBlink = millis();
-        digitalWrite(LED_BUILTIN, state);
-    }
 }
 
 void loop() {
@@ -63,5 +44,4 @@ void loop() {
     WiFiManager::CheckConnection(lcd);
     lcd.Update();
     deviceManager.Update();
-    status_led();
 }
